85b0d20e78871b942a21be5f0dfbb67ea813f173
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-10-20 15:39:32 +0100

    tp class

---
+++ K-Nearest_Neighbors_App/tp_class/tp2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import mean_absolute_error
+
+x_train = np.array([
+    [1.0, 0.25],
+    [0.4, 0.10],
+    [0.5, 0.50],
+    [1.0, 1.0]
+])
+
+y_train = np.array([0, 0, 1, 1])
+
+p = np.array([[0.5, 0.15]])  # Doit être 2D pour sklearn
+
+distances = np.sqrt(np.sum((x_train - p) ** 2, axis=1))
+print("Distances :", distances)
+
+k = 3  
+knn = KNeighborsClassifier(n_neighbors=k)
+knn.fit(x_train, y_train)
+
+prediction = knn.predict(p)
+print(f"Classe prédite pour le point {p} : {prediction[0]}")
+
+# ---- Visualisation ----
+plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train, cmap='coolwarm', label='Train data')
+plt.scatter(p[0, 0], p[0, 1], color='green', marker='X', s=150, label='Point à prédire')
+plt.title("KNN Classification")
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.legend()
+plt.show()
