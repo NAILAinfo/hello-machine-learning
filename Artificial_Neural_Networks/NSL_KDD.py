73da8f164d4eac044989df4fabf9e25e31a29c72
NAILAinfo <mhammed.dahmani66@gmail.com>
2025-11-13 11:27:05 +0100

    etapes 1 et 2 du tp3

--- Artificial_Neural_Networks/NSL_KDD.py
+++ Artificial_Neural_Networks/NSL_KDD.py
@@ -1,35 +1,12 @@
 import numpy as np
 import tensorflow as tf
 import random
+import pandas as pd
 #Set random seed
 np.random.seed(42)
 tf.random.set_seed(42)
 random.seed(42)
-
-#Load NSL-KDD dataset
-import pandas as pd
-
-
-columns = [
-    'duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes',
-    'land', 'wrong_fragment', 'urgent', 'hot', 'num_failed_logins', 'logged_in',
-    'num_compromised', 'root_shell', 'su_attempted', 'num_root', 'num_file_creations',
-    'num_shells', 'num_access_files', 'num_outbound_cmds', 'is_host_login',
-    'is_guest_login', 'count', 'srv_count', 'serror_rate', 'srv_serror_rate',
-    'rerror_rate', 'srv_rerror_rate', 'same_srv_rate', 'diff_srv_rate',
-    'srv_diff_host_rate', 'dst_host_count', 'dst_host_srv_count',
-    'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
-    'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate',
-    'dst_host_serror_rate', 'dst_host_srv_serror_rate', 'dst_host_rerror_rate',
-    'dst_host_srv_rerror_rate', 'label', 'difficulty'
-]
-
-import pandas as pd
-
-# URL du dataset
 url = "https://raw.githubusercontent.com/defcom17/NSL_KDD/master/KDDTrain+.txt"
-
-# Noms des colonnes (43 en tout)
 columns = [
     "duration","protocol_type","service","flag","src_bytes","dst_bytes",
     "land","wrong_fragment","urgent","hot","num_failed_logins",
@@ -45,6 +22,6 @@ columns = [
     "dst_host_srv_rerror_rate","label"
 ]
 df = pd.read_csv(url, names=columns)
-# Afficher les 5 premières lignes
-print(df.head())
+print(df.head())# Afficher les 5 premières lignes
 print("Shape of dataset:", df.shape)
+print(df['label'].value_counts())
